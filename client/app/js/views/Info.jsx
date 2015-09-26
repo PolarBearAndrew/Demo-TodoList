411d77a02b4d544928ba46f4e3f9204c9be619f7
Andrew <chenpoanandrew@gmail.com>
2015-09-26 17:59:08 +0800

    finish

--- client/app/js/views/Info.jsx
+++ client/app/js/views/Info.jsx
@@ -13,7 +13,25 @@ var Info = React.createClass({
 
         return (
             <div className="info">
-                <h1 className="header"> Todolist Demo for Fandora </h1>
+                <h1 className="header"> Todolist Demo</h1>
+                <hr/>
+                <h4 className="header"> Author : &nbsp;&nbsp;&nbsp;
+                  <a className="btn btn-info" href="https://github.com/PolarBearAndrew" target="_blank">
+                    <img className="icon" src="http://www.nimrodstech.com/wp-content/uploads/1403730753_github.png" />
+                    &nbsp;&nbsp; Andrew Chen
+                  </a>
+                </h4>
+
+                <h4 className="header"> Source : &nbsp;&nbsp;&nbsp;
+                  <a className="btn btn-success" href="https://github.com/PolarBearAndrew/Demo-TodoList">
+                    <img className="icon" src="http://www.nimrodstech.com/wp-content/uploads/1403730753_github.png" />
+                    &nbsp;&nbsp; Github Repository
+                  </a>
+                </h4>
+
+                <h4 className="header"> Use :
+                  <h5 className="header"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;React.js/Flux + Node.js + MySQL + AWS</h5>
+                </h4>
             </div>
         );
     },
